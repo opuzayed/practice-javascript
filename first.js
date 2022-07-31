3b55d650274dd9944343e0692fb39d3fac4cac92
opuzayed <zayedopu4933@gmail.com>
2022-07-31 15:31:29 +0600

    shorthand added

--- first.js
+++ first.js
@@ -1,4 +1,8 @@
-var money = 500;
-var players = 10;
-var eachPlayer = money / players;
-console.log(eachPlayer);
+var tomatoPice = 20;
+tomatoPice = 30;
+var price1 = 30;
+var price2 = 40;
+var newPrice1 = price1 + 20;
+console.log(newPrice1);
+ price2 -= 20;
+console.log(price2);
