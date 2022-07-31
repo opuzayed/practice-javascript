9a060d86a802435b23f232720ba0f74dfc00b9c2
opuzayed <zayedopu4933@gmail.com>
2022-07-31 16:31:44 +0600

    see float type value

--- first.js
+++ first.js
@@ -1,3 +1,6 @@
-var gpa = 3.51;
-var gpaNumber = parseFloat(gpa);
-console.log(gpaNumber);
+var num1 = 0.1;
+var num2 = 0.2;
+var number = num1 + num2;
+var newNumber = number.toFixed(2);
+var numericNumber = parseFloat(newNumber);
+console.log(numericNumber);
