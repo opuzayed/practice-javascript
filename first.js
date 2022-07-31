a66991b63e12e4a592f0652ebe00077bc565fc18
opuzayed <zayedopu4933@gmail.com>
2022-07-31 16:08:06 +0600

    convert type value

--- first.js
+++ first.js
@@ -1,8 +1,3 @@
-var price1 = 10;
-var price2 = 20;
-var totalPrice = price1 + price2;
-// console.log(totalPrice);
-var firstName = 'zayed';
-var lastName = 'opu';
-var fullName = firstName + ' ' + lastName;
-console.log(fullName);
+var gpa = 3.51;
+var gpaNumber = parseFloat(gpa);
+console.log(gpaNumber);
