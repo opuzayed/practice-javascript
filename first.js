ad5197cdadb8b7c3c1b02e4a493bc46c036420b9
opuzayed <zayedopu4933@gmail.com>
2022-07-31 16:58:37 +0600

    learn remainder

--- first.js
+++ first.js
@@ -1,6 +1,4 @@
-var num1 = 0.1;
-var num2 = 0.2;
-var number = num1 + num2;
-var newNumber = number.toFixed(2);
-var numericNumber = parseFloat(newNumber);
-console.log(numericNumber);
+var mangoes = 13;
+var person = 5;
+var remainder = mangoes % person;
+console.log(remainder);
