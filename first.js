782442ef31da24f575867aa0babe4afd44ce7dd1
opuzayed <zayedopu4933@gmail.com>
2022-08-02 14:47:23 +0600

    added first element in the beggining

--- first.js
+++ first.js
@@ -1,4 +1,10 @@
-var numericValue = [1, 2, 3];
-var names = ['opu', 'rakib', 'rony'];
-var positionName = names[1];
-console.log(positionName);
+// var numericValue = [1, 2, 3, 4, 5];
+// var names = ['opu', 'rakib', 'rony'];
+// var positionName = names[1];
+// numericValue[2] = 30;
+// numericValue[3] = 40;
+// console.log(numericValue);
+var numbers = [1, 2, 3, 4, 5];
+numbers.unshift(6, 7);
+console.log(numbers);
+
