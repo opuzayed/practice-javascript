48c396fb6f09a4c9769e56a84c02d18e7eeaed7b
opuzayed <zayedopu4933@gmail.com>
2022-08-02 15:11:16 +0600

    check 3 condition

--- first.js
+++ first.js
@@ -8,13 +8,33 @@ var numbers = [1, 2, 3, 4, 5];
 numbers.unshift(6, 7);
 console.log(numbers);
 // condition
-var isGraduated = true;
-var salary = 25000;
-if(isGraduated == false && salary > 50000)
+// var isGraduated = true;
+// var salary = 25000;
+// if(isGraduated == false && salary > 50000)
+// {
+//     console.log("i'll marry you");
+// }
+// else{
+//     console.log("i'll not marry you");
+// }
+// -----------another-condition---------------
+var money = 100;
+var pizza = 100;
+var biscuit = 50;
+var bon = 30;
+if(pizza <= money)
 {
-    console.log("i'll marry you");
+    console.log('pizza khabo');
+}
+else if(biscuit < money)
+{
+    console.log('biscuit khabo');
+}
+else if(bon < money)
+{
+    console.log('bon khabo');
 }
 else{
-    console.log("i'll not marry you");
+    console.log('cha khobo');
 }
 
