6281cc851090250e70cec710a632cf00cd5a814b
opuzayed <zayedopu4933@gmail.com>
2022-08-04 14:31:31 +0600

    object

--- first.js
+++ first.js
@@ -18,23 +18,59 @@ console.log(numbers);
 //     console.log("i'll not marry you");
 // }
 // -----------another-condition---------------
-var money = 100;
-var pizza = 100;
-var biscuit = 50;
-var bon = 30;
-if(pizza <= money)
+// var money = 100;
+// var pizza = 100;
+// var biscuit = 50;
+// var bon = 30;
+// if(pizza <= money)
+// {
+//     console.log('pizza khabo');
+// }
+// else if(biscuit < money)
+// {
+//     console.log('biscuit khabo');
+// }
+// else if(bon < money)
+// {
+//     console.log('bon khabo');
+// }
+// else{
+//     console.log('cha khobo');
+// }
+// ----------------------------summary-------------
+// var destinations = ['dhaka', 'lakshmipur', 'barishal', 'dubai', 'sylhet'];
+// console.log(destinations.indexOf('lakshmipur'));
+// destinations[2] = 'noakhali';
+
+// console.log(destinations);
+/* var marks = prompt('Enter your marks : ')
+if(marks >= 80 && marks <= 100)
 {
-    console.log('pizza khabo');
+    console.log('your grade is A ');
 }
-else if(biscuit < money)
+else if(marks >= 60 && marks <=79)
 {
-    console.log('biscuit khabo');
+console.log('your grade is B');
 }
-else if(bon < money)
+else if(marks >= 50 && marks <= 59)
 {
-    console.log('bon khabo');
+console.log('your grade is C');
 }
-else{
-    console.log('cha khobo');
+else if(marks >= 40 && marks <= 49)
+{
+console.log('your grade is D')
+}
+else
+{
+    console.log('You are fail in exam');
 }
 
+ */
+// ------------------object---------------
+var mobile = {
+    brand : 'nokia',
+    price : 2300,
+    ram : '4gb',
+    processor : 'i7'
+}
+console.log(mobile.price);
